b3b7b97d4fdf35e640b4fffde9ac5d8f6c866e37
orayanics <nicole.oraya.cics@ust.edu.ph>
2024-09-27 13:23:26 +0800

    run concurrently from root
    $talktherapy-cap: npm run dev
    - need the env

---
+++ server/index.js
@@ -0,0 +1,63 @@
+require("dotenv").config();
+
+const config = require("./config.json");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(config.connectionString)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+  });
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const path = require("path");
+
+const patientSlpRoutes = require("./routes/patientSlp.route.js");
+const clinicianSLPRoutes = require("./routes/clinicianSLP.route.js");
+const adminSLPRoutes = require("./routes/adminSLP.route.js");
+const superAdminSLPRoutes = require("./routes/superAdminSLP.route.js");
+const scheduleRoutes = require("./routes/schedule.route.js");
+const contentRoute = require("./routes/content.route.js");
+
+app.use(express.json());
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+// Middleware to log requests
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+// Use the routes
+
+app.use("/patient-slp", patientSlpRoutes);
+app.use("/clinicianSLP", clinicianSLPRoutes);
+app.use("/adminSLP", adminSLPRoutes);
+app.use("/super-admin", superAdminSLPRoutes);
+app.use("/schedule", scheduleRoutes);
+app.use("/api/contents", contentRoute);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+app.get("/", (req, res) => {
+  res.json({ data: "hello" });
+});
+
+app.listen(8000, () => {
+  console.log("Server is running on port 8000");
+});
