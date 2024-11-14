a273c63db94853d1b320e0243a6bd72e77f41289
Reyko2 <rico.nieto.cics@ust.edu.ph>
2024-11-14 10:08:22 +0800

    Update auditLog.js

--- server/middleware/auditLog.js
+++ server/middleware/auditLog.js
@@ -1,11 +1,8 @@
 const AuditLog = require("../models/auditLogSLP.model.js");
 
 const createAuditLog = async (action, user, details) => {
-  // const auditLog = new AuditLog({ action, user, details });
-  // await auditLog.save();
-  console.log(
-    `Audit log action: ${action}, user: ${user}, details: ${details}`
-  );
+  const auditLog = new AuditLog({ action, user, details });
+  await auditLog.save();
 };
 
 module.exports = { createAuditLog };
