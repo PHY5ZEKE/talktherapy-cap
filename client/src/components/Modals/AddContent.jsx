c1dd18153f8c869f7cf40c5873fb7196a097e8a2
Ezequiel Gonzalez <ezequiel.gonzalez.cics@ust.edu.ph>
2024-11-25 20:40:42 +0800

    bdnsada

--- client/src/components/Modals/AddContent.jsx
+++ client/src/components/Modals/AddContent.jsx
@@ -70,7 +70,14 @@ export default function AddContent({ closeModal, onSubmit }) {
 
   // Handle file selection
   const handleImageChange = (e) => {
-    setImage(e.target.files[0]);
+    const file = e.target.files[0];
+    if (file && file.size > 1 * 1024 * 1024) {
+      // Check if file size exceeds 5 MB
+      toast.error("Image must not exceed 5 MB");
+      setImage(null); // Reset image input
+      return;
+    }
+    setImage(file);
   };
 
   const modules = {
